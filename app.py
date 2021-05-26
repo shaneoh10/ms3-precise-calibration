42bcb07e69d53ed5c65dd89b2be10f931e80bd10
shaneoh10 <shaneaghada@gmail.com>
2021-05-26 10:34:31 +0000

    feat: Add cals-due.html, connect Flask to MongoDB

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, request, session, 
+    redirect, url_for, render_template)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def welome():
-    return "Welcome to Precise Calibration"
+@app.route("/get_cals_due")
+def get_cals_due():
+    cals_due = mongo.db.cals_due.find()
+    return render_template("cals-due.html", cals_due=cals_due)
 
 
 if __name__ == "__main__":
