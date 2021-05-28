ec2a4ae3025307245ea0daa886e2fe6be9c63ba2
shaneoh10 <shaneaghada@gmail.com>
2021-05-28 07:59:11 +0000

    feat: Add JS effects to home page

---
+++ static/js/homepage.js
@@ -0,0 +1,25 @@
+  // Navbar appear on scroll up
+  // https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+          document.getElementById("navbar").style.top = "0";
+      } else {
+          document.getElementById("navbar").style.top = "-80px";
+      }
+      prevScrollpos = currentScrollPos;
+  }
+
+  // Divs appear on scroll down
+  // https://scrollrevealjs.org/guide/whats-new.html
+
+  ScrollReveal().reveal('.about-card', {
+      duration: 3000,
+      delay: 200
+  });
+  ScrollReveal().reveal('.contact-card', {
+      duration: 3000,
+      delay: 200
+  });
